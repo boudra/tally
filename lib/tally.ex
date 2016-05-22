f703cd6b1cecb3b712e3f2fc3ffa64d6fd6df484
Mohamed Boudra <boudra.moha@gmail.com>
2016-05-22 19:02:32 +0200

    basic layer based reporting

--- lib/tally.ex
+++ lib/tally.ex
@@ -1,2 +1,41 @@
 defmodule Tally do
+
+  defp do_report(module, layer, args \\ :empty) do
+    apply(module, layer, [args] |> Enum.reject(&(&1 == :empty)))
+  end
+
+  def report(module, name) do
+    { ^name, layers } = List.keyfind(module.__reports__, name, 0)
+    Enum.reduce(layers, :empty, fn(current, acc) ->
+      do_report(module, current, acc)
+    end)
+  end
+
+end
+
+defmodule Tally.Reporter do
+
+  defmacro __before_compile__(_) do
+    quote do
+      def __reports__ do
+        @reports
+      end
+    end
+  end
+
+  defmacro __using__(_) do
+    quote do
+      import Tally.Reporter
+      Module.register_attribute(__MODULE__, :reports, accumulate: true)
+      @before_compile Tally.Reporter
+    end
+  end
+
+  defmacro report(name, layers) do
+    layers = [ layers ] |> List.flatten
+    quote do
+      @reports { unquote(name), unquote(layers) }
+    end
+  end
+
 end
--- test/tally_test.exs
+++ test/tally_test.exs
@@ -1,8 +1,38 @@
+defmodule Reports do
+
+  use Tally.Reporter
+
+  @sales [
+    %{ amount: 12.4, currency: "GBP" },
+    %{ amount: 85.3, currency: "EUR" },
+    %{ amount: 15.2, currency: "EUR" },
+    %{ amount: 25.1, currency: "USD" },
+  ]
+
+  def sales do
+    @sales
+  end
+
+  def currency_avg(sales) do
+    sales
+    |> Enum.group_by(&(Map.get(&1, :currency)))
+    |> Enum.map(fn({currency, sales}) ->
+      [{currency, Enum.reduce(sales, 0, &(&1.amount + &2)) / Enum.count(sales)}] |> Map.new
+    end)
+  end
+
+  report "sales", :sales
+  report "currency averages", [ :sales, :currency_avg ]
+end
+
 defmodule TallyTest do
   use ExUnit.Case
   doctest Tally
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "layers" do
+    assert Tally.report(Reports, "sales") == Reports.sales
+    assert Tally.report(Reports, "currency averages") |> Enum.count == 3
+    assert Tally.report(Reports, "currency averages") == [%{"EUR" => 50.25}, %{"GBP" => 12.4}, %{"USD" => 25.1}]
   end
+
 end
